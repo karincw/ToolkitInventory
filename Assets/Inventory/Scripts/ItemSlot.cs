881fca3969b1cd39b9efa6a4067c73d1f2e11a2d
Karin <karin.cw07@gmail.com>
2023-11-22 11:40:30 +0900

    start

---
+++ Assets/Inventory/Scripts/ItemSlot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Karin.Inventory
+{
+    public class ItemSlot : VisualElement
+    {
+
+    }
+}
